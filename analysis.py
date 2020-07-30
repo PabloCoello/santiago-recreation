568004278c2cfd4defe4da447490f56741785890
Pablo Coello <pablo.coellopulido@outlook.es>
2020-07-30 18:34:13 +0200

    Update

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import json
 import datetime
+import numpy as np
 
 
 with open('./data/santiago.json') as f:
@@ -35,18 +36,28 @@ gdf = get_gdf(data)
 df = pd.DataFrame(gdf)
 df.to_excel('./data/excel.xlsx')
 
-monumentos = pd.read_excel('./data/monumentos.xlsx')
-monumentos = gpd.GeoDataFrame(monumentos, geometry=gpd.points_from_xy(monumentos.longitud, monumentos.latitud),
+monumentos2 = pd.read_excel('./data/monumentos.xlsx')
+monumentos2 = gpd.GeoDataFrame(monumentos, geometry=gpd.points_from_xy(monumentos.longitud, monumentos.latitud),
                               crs={'init' :'epsg:4326'})
-monumentos = monumentos.to_crs(epsg=3763)
-monumentos['geometry'] = monumentos.buffer(50)
-monumentos = monumentos.to_crs(epsg=4326)
+monumentos2 = monumentos.to_crs(epsg=3763)
+monumentos2['geometry'] = monumentos2.buffer(150)
+monumentos2 = monumentos2.to_crs(epsg=4326)
 
-join = gpd.sjoin(gdf, monumentos, how='inner', op='intersects')
+join2 = gpd.sjoin(gdf, monumentos2, how='left', op='intersects')
+cidade = join2[join2['Nombre']=='Cidade da Cultura']
+cidade['id'].unique()
+
+join.iloc[join['Nombre'].isnull().values, join.columns.get_indexer([
+    'Nombre'])] = 'Sen identificar'
+join.iloc[join['id'].isin(cidade['id'].unique()).values, join.columns.get_indexer([
+    'Nombre'])] = 'Cidade da Cultura'
+
+join.to_excel('/mnt/c/Users/Pablo/Desktop/Santiagojoin.xlsx')
+
+
+df = pd.read_excel('./data/join.xlsx')
 
 group = join.groupby('Nombre')
 count = group.count()['id']
 total_rel_count = (count/953) * 100
 relative_rel_count = (count/480) * 100
-
-join.to_excel('./data/join.xlsx')
