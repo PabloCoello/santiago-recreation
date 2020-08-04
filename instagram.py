2ec88abc1cd011e357bd53cb91284ea2ef122081
Pablo Coello <pablo.coello@outlook.com>
2020-08-04 13:21:03 +0200

    Added geocoding

--- instagram.py
+++ instagram.py
@@ -2,10 +2,14 @@ import requests
 import time
 import json
 import re
-from pymongo import MongoClient
+from pymongo import MongoClient, GEOSPHERE
 import pandas as pd
 import datetime
 import progressbar
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+from functools import partial
+from shapely.geometry import mapping
 
 
 def format_data(data):
@@ -14,8 +18,20 @@ def format_data(data):
         int(data['taken_at_timestamp']))
     return data
 
+def format_location(data, geolocator):
+    dt = json.loads(data['location']['address_json'])
+    geocode = partial(geolocator.geocode,  country_codes=dt['country_code'], geometry='geojson')
+    loc = geocode(query=dt["city_name"])
+    data['geometry'] = loc.raw['geojson']
+    data['nominatin'] = loc.raw
+    data['latitude'] = loc.latitude
+    data['longitude'] = loc.longitude
+    time.sleep(1.1)
+    return data
+
 # STEP 1: Scrap posts for a tag
 end_cursor = ''  # empty for the 1st page
+geolocator = Nominatim(user_agent="pablo.coellopulido@usc.es")
 
 with open('./conf.json', 'r') as f:
     conf = json.load(f)
@@ -26,6 +42,7 @@ if re.search(' ', conf['database']) is None and len(conf['database']) < 20:
         client = MongoClient('localhost', 27017)
         db = eval('client.' + conf['database'])
         collection = eval('db.' + conf['collection'])
+        collection.create_index([("geometry", GEOSPHERE)])
     else:
         print('invalid collection name')
 else:
@@ -44,11 +61,11 @@ for i in progressbar.progressbar(range(0, conf['page_count'])):
     # list with posts
     edges = data['graphql']['hashtag']['edge_hashtag_to_media']['edges']
 
+    time.sleep(2)  # insurence to not reach a time limit
+    
     if len(edges) > 0:
         for item in edges:
             arr.append(item['node'])
-
-        time.sleep(2)  # insurence to not reach a time limit
 
         for item in arr:
             shortcode = item['shortcode']
@@ -57,8 +74,12 @@ for i in progressbar.progressbar(range(0, conf['page_count'])):
             r = requests.get(url)
             try:
                 data = json.loads(r.text)
-                collection.insert_one(format_data(data))
+                data = format_data(data)
+                if len(data['location']['address_json']) > 0:
+                    data = format_location(data, geolocator) 
+                    collection.insert_one(data)
             except:
                 pass
 
-print(end_cursor)  # save this to restart parsing with the next page
+print(end_cursor)
+client.close()
